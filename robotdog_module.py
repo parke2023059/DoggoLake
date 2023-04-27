344dc8339464ec710c2659f5114e799c78ad3bf9
Parker Fink <parke2023059@live.ytech.edu>
2023-04-27 11:01:58 -0400

    new tts

--- robotdog_module.py
+++ robotdog_module.py
@@ -2,11 +2,11 @@ import cv2
 import openai
 import pyttsx3
 import gtts
-
-
-from playsound import playsound
-
 import os
+import json
+import requests
+
+
 
 
 
@@ -33,7 +33,7 @@ class RobotDog:
     def askQuestion():
         engine = pyttsx3.init()
 
-        openai.api_key = "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"
+        openai.api_key = "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"
         question = input("What is your question?")
         response = openai.ChatCompletion.create(
         model='gpt-3.5-turbo' ,
@@ -49,10 +49,25 @@ class RobotDog:
 
 
     def test():
-        tts = gtts.gTTS("Hola Mundo", lang="en")
-        tts.save("hola.mp3")
-        playsound("hola.mp3")
+        openai.api_key = "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"
+        question = input("wuz ur question?")
+        response = openai.ChatCompletion.create( model='gpt-3.5-turbo', messages=[ {"role": "system", "content": "Your are a helpful assistant."}, {"role": "user", "content": question}, ])
+        message = response.choices[0]['message']
+        print(message['content'])
+        url = "https://api.uberduck.ai/speak"
+        payload = { "pace": 1, "speech": message['content'] }
+        headers = { "accept": "application/json", "uberduck-id": "anonymous", "content-type": "application/json", "authorization": "Basic cHViX2xhc2doa2NncnRjb256Z2tybjpwa185OTkyZDY4YS1mNWI0LTQ1MjktYjMyNi04YTM2MjA2OGY3Mzg=" }
+        response = requests.post(url, json=payload, headers=headers)
+        print(json.loads(response.text)["uuid"])
+        url = "https://api.uberduck.ai/speak-status?uuid=" + json.loads(response.text)["uuid"]
+        headers = {"accept": "application/json"}
+        response = requests.get(url, headers=headers)
+        print(response.text)
 
+        print("The URL is: " + url)
+        #print(url + '/audio.wav')
+        link = requests.get(url + '/audio.wav')
+        open('audio.wav', 'wb').write(link)
 
 #RobotDog.startRecording()
 #RobotDog.askQuestion()
