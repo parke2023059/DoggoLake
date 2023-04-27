13a62be9568d327ffd1cd6a433d85698c7420d03
Parker Fink <parke2023059@live.ytech.edu>
2023-04-27 08:16:50 -0400

    Update robotdog_module.py

--- robotdog_module.py
+++ robotdog_module.py
@@ -6,11 +6,14 @@ class RobotDog:
 
     def startRecording():
         vid = cv2.VideoCapture(0)
+
         while(True):
             ret, frame = vid.read()
             cv2.imshow('Robot Dog', frame)
+            
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
+
         vid.release()
         cv2.destroyAllWindows()
 
