b33688a2d2402482e3c519ba89b9b71414cf198e
Parker Fink <parke2023059@live.ytech.edu>
2023-05-04 09:36:26 -0400

    more modules

--- installer.py
+++ installer.py
@@ -16,9 +16,13 @@ if OperatingSystem.name == 'Windows':
     os.system('pip install opencv-python')
     os.system('pip install openai')
     os.system('pip install pyttsx3')
+    os.system('pip install requests')
+    os.system('pip install python-dotenv')
     time.sleep(5)
 else: 
     os.system('pip3 install opencv-python')
     os.system('pip3 install openai')
     os.system('pip3 install pyttsx3')
+    os.system('pip3 install requests')
+    os.system('pip3 install python-dotenv')
     time.sleep(5)
