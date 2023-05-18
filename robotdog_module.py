880c12a2fed5fa8fabc1d44664fec4587685c011
Parker Fink <parke2023059@live.ytech.edu>
2023-05-18 11:15:20 -0400

    facial detection

--- robotdog_module.py
+++ robotdog_module.py
@@ -113,9 +113,40 @@ class RobotDog:
         client = uberduck.UberDuck(apiKey, )
 
 
+    def facialRecognition():
+        face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+        eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
+        cap = cv2.VideoCapture(0)
+
+        while 1:
+
+            ret, img = cap.read()
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+            for (x,y,w,h) in faces:
+                cv2.rectangle(img,(x,y),(x+w,y+h),(255,255,0),2) 
+                roi_gray = gray[y:y+h, x:x+w]
+                roi_color = img[y:y+h, x:x+w]
+
+                eyes = eye_cascade.detectMultiScale(roi_gray) 
 
 
-RobotDog.startRecording()
+                for (ex,ey,ew,eh) in eyes:
+                    cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,127,255),2)
+
+            cv2.imshow('Victim Camera',img)
+
+            k = cv2.waitKey(30) & 0xff
+            if k == 27:
+                break
+
+
+        cap.release()
+        cv2.destroyAllWindows() 
+
+
+#RobotDog.startRecording()
 #RobotDog.askQuestion()
 #RobotDog.askQuestion2()
 #RobotDog.play_audio()
+RobotDog.facialRecognition()
